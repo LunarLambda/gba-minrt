8868209d7a0486da82071705c084303e0ca5a75c
LunarLambda <lunarlambda@gmail.com>
2022-10-16 15:53:32 +0200

    merge branch meson

--- src/ram.c
+++ src/ram.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <minrt.h>
 
 extern char __iwram_size;
@@ -17,7 +18,7 @@ size_t minrt_used_stack(void)
 {
     uintptr_t sp;
 
-    __asm__("mov %0, sp" : =r(sp));
+    __asm__("mov %0, sp" : "=r"(sp));
 
     return 0x03007F00 - sp;
 }
@@ -26,7 +27,7 @@ ptrdiff_t minrt_stack_left(void)
 {
     intptr_t sp;
 
-    __asm__("mov %0, sp" : =r(sp));
+    __asm__("mov %0, sp" : "=r"(sp));
 
     return sp - ((intptr_t)&__iwram_size + 0x03000000);
 }
